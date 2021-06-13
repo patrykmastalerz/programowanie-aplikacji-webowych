f54ca644512312d8563f9bde51e7c2c4186a96c7
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-06-13 12:01:49 +0200

    jest

---
+++ lab4/notekeep/tests/note.test.ts
@@ -0,0 +1,12 @@
+import { Note } from '../src/Note';
+import { Notes } from '../src/Notes';
+import { DatabaseConfiguration } from '../src/configDatabase';
+import { IAppStorage } from '../src/IAppStorage';
+
+describe('Fibonacci 2', () => {
+    let note: Note;
+    let database: IAppStorage;
+
+
+
+})
