7066584ea7ec6bd68ca461d68e0f086e6e4b1b89
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-06-13 12:30:04 +0200

    dodanie AppStorage mocka

---
+++ lab4/notekeep/tests/AppStorageMock.ts
@@ -0,0 +1,32 @@
+import { IAppStorage } from '../src/IAppStorage';
+import { INoteInterface } from '../src/INoteInterface';
+
+export class AppStorageMock implements IAppStorage{
+    async saveData(note: INoteInterface){
+        let existingNotes = await this.getData();
+        existingNotes.push(note);
+
+        localStorage.setItem(this.KEY_NOTES, JSON.stringify(existingNotes));
+    }
+    
+    async getData(): Promise<INoteInterface[]> {
+        const data = localStorage.getItem(this.KEY_NOTES);
+
+        if (data) 
+            return JSON.parse(data);
+        return [];
+    }
+
+    async updateData(id: number, title: string, text: string, pinned: boolean){
+        let existingNotes = await this.getData();
+
+        const result = existingNotes.map(item => item.id === id ? { ...item, title, text, pinned } : item)
+        localStorage.setItem(this.KEY_NOTES, JSON.stringify(result));
+    }
+
+    async removeData(id: number){
+        const notes = await this.getData();
+        notes.splice(notes.findIndex((n) => n.id === id), 1);
+        localStorage.setItem(this.KEY_NOTES, JSON.stringify(notes));
+    }
+}
