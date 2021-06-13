6e58ede0b58e53d6605799cdbf227fb86ee75e9f
Patryk Mastalerz <patryk.mastalerz@microsoft.wsei.edu.pl>
2021-06-13 12:51:29 +0200

    pierwszy test

--- lab4/notekeep/tests/AppStorageMock.ts
+++ lab4/notekeep/tests/AppStorageMock.ts
@@ -2,31 +2,33 @@ import { IAppStorage } from '../src/IAppStorage';
 import { INoteInterface } from '../src/INoteInterface';
 
 export class AppStorageMock implements IAppStorage{
-    async saveData(note: INoteInterface){
-        let existingNotes = await this.getData();
-        existingNotes.push(note);
 
-        localStorage.setItem(this.KEY_NOTES, JSON.stringify(existingNotes));
+    private readonly KEY_NOTES : string = 'notes'; 
+
+    async saveData(note: INoteInterface){
+
     }
     
     async getData(): Promise<INoteInterface[]> {
         const data = localStorage.getItem(this.KEY_NOTES);
 
         if (data) 
-            return JSON.parse(data);
+            return [{
+                id: 1,
+                title: "tytul", 
+                text: 'tekst',
+                color: 'white',
+                date: '10-10-2010',
+                pinned: true
+            }]
         return [];
     }
 
     async updateData(id: number, title: string, text: string, pinned: boolean){
-        let existingNotes = await this.getData();
 
-        const result = existingNotes.map(item => item.id === id ? { ...item, title, text, pinned } : item)
-        localStorage.setItem(this.KEY_NOTES, JSON.stringify(result));
     }
 
     async removeData(id: number){
-        const notes = await this.getData();
-        notes.splice(notes.findIndex((n) => n.id === id), 1);
-        localStorage.setItem(this.KEY_NOTES, JSON.stringify(notes));
+
     }
 }
